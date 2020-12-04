6a7211721619a3543f7bd0039012ae0bad35e909
Igor Asamadzinau <igor.asamadzinau@fmr.com>
2020-12-04 09:56:32 -0500

    Initial commit

---
+++ src/main/java/pluralsight/demo/springcloud/clientdemo/PluralsightSpringcloudClientDemoApplication.java
@@ -0,0 +1,13 @@
+package pluralsight.demo.springcloud.clientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluralsightSpringcloudClientDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pluralsight/demo/springcloud/clientdemo/PluralsightSpringcloudClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pluralsight.demo.springcloud.clientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightSpringcloudClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
